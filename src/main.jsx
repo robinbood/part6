c5ef0a136545687b8ed159be72809907c45048f8
robinbood <mercedesfarts@gmail.com>
2025-02-26 21:56:12 +0500

    fixing it

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const client = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <QueryClientProvider client={client}>
+    <App />
+  </QueryClientProvider>
 )
